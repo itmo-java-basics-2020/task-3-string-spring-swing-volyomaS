21c5ce8b7c67b5263ae5a3eac45341578c0c7412
Daniil <rastoden@gmail.com>
2020-02-29 13:19:38 +0300

    lab3 done, fixed codestyle

--- src/main/java/ru/itmo/java/Task3.java
+++ src/main/java/ru/itmo/java/Task3.java
@@ -11,14 +11,17 @@ public class Task3 {
      * Если инпут равен null - вернуть пустой массив
      */
     int[] getShiftedArray(int[] inputArray) {
-        if (inputArray == null) return new int[0];
-        if (inputArray.length == 1 || inputArray.length == 0) return inputArray;
+        if (inputArray == null) {
+            return new int[0];
+        }
+        if (inputArray.length == 1 || inputArray.length == 0) {
+            return inputArray;
+        }
         int prev = inputArray[0];
         int temp;
-        for (int i = 0; i < inputArray.length - 1; ++i)
-        {
-            temp = inputArray[i+1];
-            inputArray[i+1] = prev;
+        for (int i = 0; i < inputArray.length - 1; ++i) {
+            temp = inputArray[i + 1];
+            inputArray[i + 1] = prev;
             prev = temp;
         }
         inputArray[0] = prev;
@@ -28,71 +31,78 @@ public class Task3 {
     /**
      * Напишите функцию, которая принимает массив целых чисел и возвращает максимальное значение произведения двух его элементов.
      * Если массив состоит из одного элемента, то функция возвращает этот элемент.
-     *
+     * <p>
      * Если входной массив пуст или равен null - вернуть 0
-     *
+     * <p>
      * Пример: 2 4 6 -> 24
      */
     int getMaxProduct(int[] inputArray) {
-        if (inputArray == null || inputArray.length == 0) return 0;
-        if (inputArray.length == 1) return inputArray[0];
+        if (inputArray == null || inputArray.length == 0) {
+            return 0;
+        }
+        if (inputArray.length == 1) {
+            return inputArray[0];
+        }
         int max = Math.max(inputArray[1], inputArray[0]);
         int pmax = Math.min(inputArray[1], inputArray[0]);
         int min = Math.min(inputArray[1], inputArray[0]);
         int pmin = Math.max(inputArray[1], inputArray[0]);
         for (int i = 2; i < inputArray.length; ++i) {
             int el = inputArray[i];
-            if (el > max)
-            {
+            if (el > max) {
                 pmax = max;
                 max = el;
-            }
-            else if (el > pmax)
-            {
+            } else if (el > pmax) {
                 pmax = el;
-            }
-            else if (el < min)
-            {
+            } else if (el < min) {
                 pmin = min;
                 min = el;
-            }
-            else if (el < pmin)
-            {
+            } else if (el < pmin) {
                 pmin = el;
             }
         }
-        if (pmin > 0 || min > 0) return max * pmax;
-        return Math.max(max*pmax, min*pmin);
+        if (pmin > 0 || min > 0) {
+            return max * pmax;
+        }
+        return Math.max(max * pmax, min * pmin);
     }
 
     /**
      * Напишите функцию, которая вычисляет процент символов 'A' и 'B' (латиница) во входной строке.
      * Функция не должна быть чувствительна к регистру символов.
      * Результат округляйте путем отбрасывания дробной части.
-     *
+     * <p>
      * Пример: acbr -> 50
      */
     int getABpercentage(String input) {
-        if (input == null || input.length() == 0) return 0;
+        if (input == null || input.length() == 0) {
+            return 0;
+        }
         float abcount = 0;
         String pattern = "ABab";
         for (int i = 0; i < input.length(); ++i) {
             String ch = Character.toString(input.charAt(i));
-            if (pattern.contains(ch))
+            if (pattern.contains(ch)) {
                 abcount++;
+            }
         }
-        return (int)(100*abcount/input.length());
+        return (int) (100 * abcount / input.length());
     }
 
     /**
      * Напишите функцию, которая определяет, является ли входная строка палиндромом
      */
     boolean isPalindrome(String input) {
-        if (input == null) return false;
-        if (input.length() == 0 || input.length() == 1) return true;
-        for (int i = 0; i < input.length()/2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - i - 1))
+        if (input == null) {
+            return false;
+        }
+        if (input.length() == 0 || input.length() == 1) {
+            return true;
+        }
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - i - 1)) {
                 return false;
+            }
         }
         return true;
     }
@@ -102,16 +112,20 @@ public class Task3 {
      * где группы одинаковых символов заменены на один символ и кол-во этих символов идущих подряд в строке
      */
     String getEncodedString(String input) {
-        if (input == null) return "";
-        if (input.length() == 0) return "";
+        if (input == null) {
+            return "";
+        }
+        if (input.length() == 0) {
+            return "";
+        }
         char cc = input.charAt(0);
         int count = 0;
         StringBuilder ans = new StringBuilder();
         for (int i = 0; i < input.length(); ++i) {
-            if (input.charAt(i) == cc)
+            if (input.charAt(i) == cc) {
                 count++;
-            else
-            {
+            }
+            else {
                 ans.append(cc);
                 ans.append(count);
                 count = 1;
@@ -131,22 +145,29 @@ public class Task3 {
      * isPermutation("abc", "Abc") == false;
      */
     boolean isPermutation(String one, String two) {
-        if (one == null || two == null) return false;
-        if (one.length() == 0 || two.length() == 0) return false;
-        if (one.length() != two.length()) return false;
+        if (one == null || two == null) {
+            return false;
+        }
+        if (one.length() == 0 || two.length() == 0) {
+            return false;
+        }
+        if (one.length() != two.length()) {
+            return false;
+        }
         Map<Character, Integer> dict = new HashMap<>();
         for (int i = 0; i < one.length(); i++) {
-            if (dict.containsKey(one.charAt(i)))
+            if (dict.containsKey(one.charAt(i))) {
                 dict.put(one.charAt(i), dict.get(one.charAt(i)) + 1);
-            else
+            } else {
                 dict.put(one.charAt(i), 1);
+            }
         }
         for (int i = 0; i < two.length(); i++) {
-            if (dict.containsKey(two.charAt(i)) && dict.get(two.charAt(i)) != 0)
-            {
+            if (dict.containsKey(two.charAt(i)) && dict.get(two.charAt(i)) != 0) {
                 dict.put(two.charAt(i), dict.get(two.charAt(i)) - 1);
+            } else {
+                return false;
             }
-            else return false;
         }
         return true;
     }
@@ -157,13 +178,18 @@ public class Task3 {
      * Строкой является последовательность символов длинной N, где N > 0
      */
     boolean isUniqueString(String s) {
-        if (s == null) return false;
-        if (s.length() == 0) return false;
+        if (s == null) {
+            return false;
+        }
+        if (s.length() == 0) {
+            return false;
+        }
         for (int i = 0; i < s.length() - 1; i++) {
             char c = s.charAt(i);
-            for (int j = i+1; j < s.length(); j++) {
-                if (c == s.charAt(j))
+            for (int j = i + 1; j < s.length(); j++) {
+                if (c == s.charAt(j)) {
                     return false;
+                }
             }
         }
         return true;
@@ -171,15 +197,21 @@ public class Task3 {
 
     /**
      * Напишите функцию, которая транспонирует матрицу. Только квадратные могут быть на входе.
-     *
+     * <p>
      * Если входной массив == null - вернуть пустой массив
      */
     int[][] matrixTranspose(int[][] m) {
-        if (m == null) return new int[2][0];
-        if (m.length == 0) return new int[2][0];
-        if (m[0].length == 0) return new int[2][0];
+        if (m == null) {
+            return new int[2][0];
+        }
+        if (m.length == 0) {
+            return new int[2][0];
+        }
+        if (m[0].length == 0) {
+            return new int[2][0];
+        }
         for (int i = 0; i < m.length; i++) {
-            for (int j = i+1; j < m.length; j++) {
+            for (int j = i + 1; j < m.length; j++) {
                 int c = m[i][j];
                 m[i][j] = m[j][i];
                 m[j][i] = c;
@@ -191,32 +223,41 @@ public class Task3 {
     /**
      * Напиишите функцию, принимающую массив строк и символ-разделитель,
      * а возвращает одну строку состоящую из строк, разделенных сепаратором.
-     *
+     * <p>
      * Запрещается пользоваться функций join
-     *
+     * <p>
      * Если сепаратор == null - считайте, что он равен ' '
      * Если исходный массив == null -  вернуть пустую строку
      */
     String concatWithSeparator(String[] inputStrings, Character separator) {
-        if (separator == null) separator = ' ';
-        if (inputStrings == null) return "";
-        if (inputStrings.length == 0) return "";
+        if (separator == null) {
+            separator = ' ';
+        }
+        if (inputStrings == null) {
+            return "";
+        }
+        if (inputStrings.length == 0) {
+            return "";
+        }
         StringBuilder ans = new StringBuilder();
         for (int i = 0; i < inputStrings.length - 1; i++) {
             ans.append(inputStrings[i]).append(separator);
         }
-        return ans.append(inputStrings[inputStrings.length-1]).toString();
+        return ans.append(inputStrings[inputStrings.length - 1]).toString();
     }
 
     /**
      * Напишите функцию, принимающую массив строк и строку-перфикс и возвращающую кол-во строк массива с данным префиксом
      */
     int getStringsStartWithPrefix(String[] inputStrings, String prefix) {
-        if (prefix == null || inputStrings == null || inputStrings.length == 0) return 0;
+        if (prefix == null || inputStrings == null || inputStrings.length == 0) {
+            return 0;
+        }
         int count = 0;
-        for (String el: inputStrings) {
-            if (el.length() >= prefix.length() && el.substring(0, prefix.length()).equals(prefix))
+        for (String el : inputStrings) {
+            if (el.length() >= prefix.length() && el.substring(0, prefix.length()).equals(prefix)) {
                 count++;
+            }
         }
         return count;
     }
